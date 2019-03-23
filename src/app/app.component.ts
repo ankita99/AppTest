aed45fcf9f3fdc083de27458994dc0cba0f59577
rahul7100 <46070054+rahul7100@users.noreply.github.com>
2019-03-23 23:40:10 +0530

    Added translations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ThemeService } from './shared/services/theme/theme.service';
+import { TranslateService, LangChangeEvent, DefaultLangChangeEvent  } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,13 @@ export class AppComponent {
   title = 'inventory-ui';
   darkTheme =  new FormControl(false);
 
-  constructor(private themeService: ThemeService) {
+
+  constructor(private themeService: ThemeService,private _changeDetectorRef: ChangeDetectorRef,private translate: TranslateService) {
+    this.translate.setDefaultLang('en');
+
+    const browserLang = this.translate.getBrowserLang();
+    this.translate.use(browserLang.match(/en|de|fr/) ? browserLang : 'en');
+
     this.darkTheme.valueChanges.subscribe(value => {
       if (value) {
         this.themeService.toggleDark();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { LoginComponent } from './login/login.component';
 import { ThemeService } from './shared/services/theme/theme.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+// import ngx-translate and the http loader
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,21 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule
+    BrowserModule, ReactiveFormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
